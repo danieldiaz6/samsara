77df539eac2bb53abeba22ec923eddfe6381899f
danieldiaz6 <danield006@gmail.com>
2022-11-27 13:26:59 -0500

    Update GetHistorical.js

--- GetHistorical.js
+++ GetHistorical.js
@@ -1,6 +1,6 @@
 const sdk = require('api')('@samsara-dev-rel/v2019.01.01#bcxnht13l8p3g62d');
 
-sdk.auth('samsara_api_cWGSdteERfBXwHhc56MYKN3CH4pLvt');
+sdk.auth('samsara_api_');
 sdk.getVehicleStatsHistory({
   startTime: '2022-10-03T12%3A00%3A00Z',
   endTime: '2022-10-03T14%3A00%3A00.003Z',
